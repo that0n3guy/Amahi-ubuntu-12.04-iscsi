0c60cfb50ffa4054e5d43e7a69107e66b861beb2
that0n3guy <that0n3guy@46440.no-reply.drupal.org>
2013-08-09 09:16:30 -0500

    Added username/password and node.startup config

--- iscsi-install-login.sh
+++ iscsi-install-login.sh
@@ -9,16 +9,40 @@ function log()
   echo -e "$(date +%b\ %d\ %H:%M:%S) $(hostname -s) iscsi-install-login: $@"
 }
 
+#Ask for iscsi username
+echo "Enter iscsi username: "
+read username
 
-log "Attepnting to install open-iscsi"
+#Ask for iscsi password
+echo "Enter iscsi password: "
+read password
+
+log "Attempting to install open-iscsi"
 if ! apt-get -y install open-iscsi &> /dev/null ; then
-  log "FATAL: failed to install open-iscsi"
+  log "FATAL: failed to install open-iscsi, try doing 'sudo apt-get install open-iscsi' manually."
   exit 1
 fi
   
-log "Enableing node.startup as automatic"
-if ! sed -i 's/GRUB_CMDLINE_LINUX=.*\+/GRUB_CMDLINE_LINUX="apparmor=0"/' /etc/default/grub; then
-  log "FATAL: could not disable apparmor"
+log "Setting node.startup = automatic"
+if ! sed -i 's/# node.startup = automatic.*\+/node.startup = automatic/' /etc/iscsi/iscsid.conf; then
+  log "FATAL: could not set node.startup = automatic"
   exit 1
 fi
 
+log "Setting node.startup = manual"
+if ! sed -i 's/node.startup = manual.*\+/# node.startup = manual/' /etc/iscsi/iscsid.conf; then
+  log "FATAL: could not set node.startup = manual"
+  exit 1
+fi
+
+log "Setting node.session.auth.username"
+if ! sed -i 's/#node.session.auth.username.*\+/node.session.auth.username = $username/' /etc/iscsi/iscsid.conf; then
+  log "FATAL: could not set node.session.auth.username"
+  exit 1
+fi
+
+log "Setting node.session.auth.password"
+if ! sed -i "s/#node.session.auth.password.*\+/node.session.auth.password = $password/" /etc/iscsi/iscsid.conf; then
+  log "FATAL: could not set node.session.auth.password"
+  exit 1
+fi
